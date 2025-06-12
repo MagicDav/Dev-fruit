6eea4053843568b276672b92554eee4a56155d9d
MagicDav <domingosluandakalepa@gmail.com>
2025-06-12 09:16:43 +0100

    Magic-commit-update

--- index.js
+++ index.js
@@ -4,31 +4,30 @@ const next = document.getElementById("next")
 const list = document.querySelectorAll('.item');
 
 
-let  count = list.length
+let count = list.length
 
 let active = 0
 //next.addEventListener('click', (e) => {
-  //  e.preventDefault()
+//  e.preventDefault()
 
-    //item.classList = "active";
+//item.classList = "active";
 
 //})
 
-
-next.onclick = ()=>{
-    let  activeOld = document.querySelector(".active")
+next.onclick = () => {
+    let activeOld = document.querySelector(".active")
     activeOld.classList.remove('active');
-    
-    active = active >= count -1 ? 0 : active + 1
+
+    active = active >= count - 1 ? 0 : active + 1
 
     list[active].classList.add("active")
 }
 //--------------------------------------------------------------------------////
-prev.onclick = ()=>{
-    let  activeOld = document.querySelector(".active")
+prev.onclick = () => {
+    let activeOld = document.querySelector(".active")
     activeOld.classList.remove('active');
 
-    active = active <= 0 ? count -1: active - 1
+    active = active <= 0 ? count - 1 : active - 1
 
     list[active].classList.add('active')
 }
